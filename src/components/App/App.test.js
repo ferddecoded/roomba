7914505f5f915f149b834b64261958dc3a8bc55c
ferdinand.ismael <ferdinand.ismael@achievers.com>
2022-06-06 21:02:36 -0400

    add more tests for app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './';
 
 test('renders App grid', () => {
@@ -11,4 +12,32 @@ test('renders App grid', () => {
   expect(
     screen.getByRole('button', { name: /Move forward/i }),
   ).toBeInTheDocument();
+});
+
+test('pressing turn right button updates the direction emoji', () => {
+  render(<App />);
+  expect(screen.queryAllByTestId('cell')[0].textContent).toBe('👉');
+
+  userEvent.click(screen.getByRole('button', { name: /Turn right/i }));
+  expect(screen.queryAllByTestId('cell')[0].textContent).toBe('👇');
+
+  userEvent.click(screen.getByRole('button', { name: /Turn right/i }));
+  expect(screen.queryAllByTestId('cell')[0].textContent).toBe('👈');
+
+  userEvent.click(screen.getByRole('button', { name: /Turn right/i }));
+  expect(screen.queryAllByTestId('cell')[0].textContent).toBe('☝️');
+});
+
+test('pressing move forward button updates emoji to its next location', async () => {
+  render(<App />);
+  expect(screen.queryAllByTestId('cell')[0].textContent).toBe('👉');
+
+  await waitFor(() => {
+    userEvent.click(screen.getByRole('button', { name: /move forward/i }));
+  });
+  expect(screen.queryAllByTestId('cell')[0].textContent).toBe('');
+
+  // this is the cell directly to the right of the first cell
+  // column: 1, row: 0
+  expect(screen.queryAllByTestId('cell')[10].textContent).toBe('👉');
 });
